483446b8caa87d0e08f098f7e863a593d1e4332b
guneyhan221 <guneybigay@gmail.com>
2024-07-06 22:47:19 +0300

    Create test and create email.py

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///database.db'  # SQLite veritabanı için URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # SQLite veritabanı için URI
 db = SQLAlchemy(app)
-                
+
+@app.route("home")
+def home():
+    ...
+    
 app.run(debug=True)
