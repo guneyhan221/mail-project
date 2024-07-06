324ecc1a3bf143d3fe35b696f60c4f2048c9689c
guneyhan221 <guneybigay@gmail.com>
2024-07-06 23:17:54 +0300

    Create email.db,userdb.db and update main.py

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # SQLite veritabanı için URI
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///userdb.db' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+user_db=SQLAlchemy(app)
+
+app.config['SQLALCHEMY_BINDS'] = {
+    'other': 'sqlite:///other.db'
+}
+
+email_db=SQLAlchemy(app)
 
 @app.route("home")
 def home():
     ...
-    
+
 app.run(debug=True)
