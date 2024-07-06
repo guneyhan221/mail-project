5d39c4802f891a555de56cc26e8261b8b2ec1eda
guneyhan221 <guneybigay@gmail.com>
2024-07-07 00:30:25 +0300

    Update

--- main.py
+++ main.py
@@ -3,31 +3,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///userdb.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///userandposts.db' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-user_db=SQLAlchemy(app)
+db=SQLAlchemy(app)
 
-app.config['SQLALCHEMY_BINDS'] = {
-    'other': 'sqlite:///posts.db'
-}
-
-post_db=SQLAlchemy(app)
-
-class User(user_db.Model):
-    id=user_db.Column(user_db.Integer,primary_key=True)
-    name=user_db.Column(user_db.String(24),nullable=False,unique=True)
-    name=user_db.Column(user_db.String(48),nullable=False)
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(24),nullable=False,unique=True)
+    password=db.Column(db.String(48),nullable=False)
 
     def __repr__(self) -> str:
         return f"<User {self.id}>"
 
 @app.before_request
 def create_databases():
-    user_db.create_all()
-    post_db.create_all()
+    db.create_all()
 
-@app.route("home")
+@app.route("/")
 def home():
     ...
 
