9b403d4adb97dab29f7d201b2542d8aa961674a7
guneyhan221 <guneybigay@gmail.com>
2024-07-07 00:23:42 +0300

    Update main.py and rename emails.db->posts.db

--- main.py
+++ main.py
@@ -9,10 +9,23 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 user_db=SQLAlchemy(app)
 
 app.config['SQLALCHEMY_BINDS'] = {
-    'other': 'sqlite:///other.db'
+    'other': 'sqlite:///posts.db'
 }
 
-email_db=SQLAlchemy(app)
+post_db=SQLAlchemy(app)
+
+class User(user_db.Model):
+    id=user_db.Column(user_db.Integer,primary_key=True)
+    name=user_db.Column(user_db.String(24),nullable=False,unique=True)
+    name=user_db.Column(user_db.String(48),nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<User {self.id}>"
+
+@app.before_request
+def create_databases():
+    user_db.create_all()
+    post_db.create_all()
 
 @app.route("home")
 def home():
