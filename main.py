eb1085a22e70a3a34e385d0ff4e64615c32f516d
guneyhan221 <guneybigay@gmail.com>
2024-07-06 22:40:15 +0300

    Update main.py and create database.db

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import *
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///database.db'  # SQLite veritabanı için URI
+db = SQLAlchemy(app)
+                
 app.run(debug=True)
