d456aeb216a2d62a48c518526f2049de4cc6c160
guneyhan221 <guneybigay@gmail.com>
2024-07-07 12:58:38 +0300

    Update

--- main.py
+++ main.py
@@ -9,19 +9,25 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(app)
 
 class User(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(24),nullable=False,unique=True)
-    password=db.Column(db.String(48),nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(24), nullable=False,unique=False)
+    password = db.Column(db.String(48), nullable=False)
 
     def __repr__(self) -> str:
         return f"<User {self.id}>"
 
+#Functions
+def get_user(id:int)->User:
+    return User.query.get_or_404(id)
+#Other decarators
 @app.before_request
 def create_databases():
     db.create_all()
 
+#Routes
 @app.route("/")
 def home():
-    ...
+    return ""
+
 
 app.run(debug=True)
