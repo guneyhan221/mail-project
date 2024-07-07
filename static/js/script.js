9bab33e1173838ecc6546d12792d0f85970016cd
guneyhan221 <guneybigay@gmail.com>
2024-07-07 20:57:34 +0300

    Update

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    var visibilityButton=document.querySelector(".visibility-image");
+    var passwordInput=document.querySelector("#password-input")
+
+    visibilityButton.addEventListener("click",()=>{
+        let path=visibilityButton.getAttribute("src")=="../static/img/visibility.png" ? "../static/img/visibility_off.png" : "../static/img/visibility.png";
+        visibilityButton.setAttribute("src",path)
+        let type=visibilityButton.getAttribute("src")=="../static/img/visibility.png" ? "password" : "text";
+        passwordInput.setAttribute("type",type);
+        
+    })
+});
