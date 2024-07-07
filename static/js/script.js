6ebd2ee26550f090fd77931d5b74e3dd54b0b3a3
guneyhan221 <guneybigay@gmail.com>
2024-07-07 21:13:14 +0300

    Update

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded",()=>{
-    var visibilityButton=document.querySelector(".visibility-image");
-    var passwordInput=document.querySelector("#password-input")
+    const visibilityButton=document.querySelector(".visibility-image");
+    const passwordInput=document.querySelector("#password-input")
+    const usernameInput=document.querySelector("input[name='username']")
+    const usernameRegex=/^(?=.*\d)[a-zA-Z0-9_]{4,24}$/;
+    const passwordRegex=/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,64}$/;
 
     visibilityButton.addEventListener("click",()=>{
         let path=visibilityButton.getAttribute("src")=="../static/img/visibility.png" ? "../static/img/visibility_off.png" : "../static/img/visibility.png";
@@ -9,4 +12,20 @@ document.addEventListener("DOMContentLoaded",()=>{
         passwordInput.setAttribute("type",type);
         
     })
+
+    passwordInput.addEventListener("input",()=>{
+        if (passwordRegex.test(passwordInput.value)){
+            passwordInput.style.borderColor="green";
+        }else{
+            passwordInput.style.borderColor="red";
+        }
+    });
+
+    usernameInput.addEventListener("input",()=>{
+        if (usernameRegex.test(usernameInput.value)){
+            usernameInput.style.borderColor="green";
+        }else{
+            usernameInput.style.borderColor="red";
+        }
+    });
 });
