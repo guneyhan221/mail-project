88854c891dce78cb9e5d0fdc31182b1a8e80cb12
guneyhan221 <guneybigay@gmail.com>
2024-07-08 14:56:46 +0300

    Update

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded",()=>{
     const visibilityButton=document.querySelector(".visibility-image");
     const passwordInput=document.querySelector("#password-input")
     const usernameInput=document.querySelector("input[name='username']")
-    const form=document.querySelector(".create-account-form")
+    const birthDateInput=document.querySelector("#birth-date");
+    const submitButton=document.querySelector("#submit-button")
     const usernameRegex=/^(?=.*\d)[a-zA-Z0-9_]{4,24}$/;
     const passwordRegex=/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,64}$/;
 
@@ -13,30 +14,65 @@ document.addEventListener("DOMContentLoaded",()=>{
         passwordInput.setAttribute("type",type);
         
     })
+    controlUsername();
+    controlPassword();
 
     passwordInput.addEventListener("input",()=>{
-        if (passwordRegex.test(passwordInput.value)){
-            passwordInput.style.borderColor="green";
-        }else{
-            passwordInput.style.borderColor="red";
-        }
+        controlPassword();
     });
 
     usernameInput.addEventListener("input",()=>{
-        if (usernameRegex.test(usernameInput.value)){
-            usernameInput.style.borderColor="green";
-        }else{
-            usernameInput.style.borderColor="red";
-        }
+        controlUsername();
     });
 
-    form.addEventListener("submit",(e)=>{
-        e.preventDefault();
-        if(!passwordRegex.test(passwordInput.value)){
-            window.location.href="/create-account";
-            alert("Şifreniz gereksinimleri karşılamıyor!");
-            return;
+    function onHover(){
+        submitButton.style="transform: scale(1.05);";
+    }
+
+    function onLeave(){
+        submitButton.style="transform: scale(1);";
+    }
+
+    function controlPassword(){
+        if (passwordRegex.test(passwordInput.value)){
+            passwordInput.style.borderColor="green";
+            if(usernameRegex.test(usernameInput.value)){
+                submitButton.disabled=false;
+                submitButton.style.backgroundColor="#313a43";
+                submitButton.addEventListener("mouseenter",onHover);
+                submitButton.addEventListener("mouseleave",onLeave);
+            }
+        }else{
+            passwordInput.style.borderColor="red";
+            submitButton.disabled=true;
+            submitButton.style.backgroundColor="#9fa8b5";
+            submitButton.removeEventListener("mouseenter",onHover);
+            submitButton.removeEventListener("mouseleave",onLeave);
         }
-        window.location.href="/submit";
-    });
+    }
+
+    function controlUsername(){
+        if (usernameRegex.test(usernameInput.value)){
+            usernameInput.style.borderColor="green";
+            if(passwordRegex.test(passwordInput.value)){
+                submitButton.disabled=false;
+                submitButton.style.backgroundColor="#313a43";
+                submitButton.addEventListener("mouseenter",onHover);
+                submitButton.addEventListener("mouseleave",onLeave);
+            }
+        }else{
+            usernameInput.style.borderColor="red";
+            submitButton.disabled=true;
+            submitButton.style.backgroundColor="#9fa8b5";
+            submitButton.removeEventListener("mouseenter",onHover);
+            submitButton.removeEventListener("mouseleave",onLeave);
+        }
+    }
+
+    function controlBirthDate(){
+        return birthDateInput.value==null;
+    }
+
+    birthDateInput.addEventListener("input",console.log(birthDateInput.value))
+    
 });
