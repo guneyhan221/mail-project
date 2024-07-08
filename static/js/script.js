50f518970fd521ffd832727179e3cd0696b42b62
Hilmi-10 <kiliccemile88@gmail.com>
2024-07-08 14:26:33 +0300

    update

--- static/js/script.js
+++ static/js/script.js
@@ -30,10 +30,13 @@ document.addEventListener("DOMContentLoaded",()=>{
         }
     });
 
-    form.addEventListener("submit",()=>{
+    form.addEventListener("submit",(e)=>{
+        e.preventDefault();
         if(!passwordRegex.test(passwordInput.value)){
             window.location.href="/create-account";
             alert("Şifreniz gereksinimleri karşılamıyor!");
+            return;
         }
+        window.location.href="/submit";
     });
 });
