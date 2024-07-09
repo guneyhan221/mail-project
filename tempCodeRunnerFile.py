dcc4191c0c8d01f3556b3200e8a5290d9094998a
Hilmi-10 <kiliccemile88@gmail.com>
2024-07-09 19:53:58 +0300

    tempCodeRunnerFile.py klasörünün boşlukları düzeltildi

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,5 @@
-    if rememberme =="on":
-                visiter_ip = request.remote_addr
-                rmipf = open("rmip.txt",'a')
-                rmipf.writelines(visiter_ip+"\n")
-                rmipf.close()
+if rememberme =="on":
+    visiter_ip = request.remote_addr
+    rmipf = open("rmip.txt",'a')
+    rmipf.writelines(visiter_ip+"\n")
+    rmipf.close()
